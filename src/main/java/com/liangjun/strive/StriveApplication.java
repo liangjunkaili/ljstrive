9d67818c3c3a44fa5aa11c999550943155ec3fa1
liangjun <jun.liang@magicabc.com.cn>
2018-04-19 14:29:04 +0800

    Initial commit

---
+++ src/main/java/com/liangjun/strive/StriveApplication.java
@@ -0,0 +1,12 @@
+package com.liangjun.strive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StriveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/liangjun/strive/StriveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liangjun.strive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StriveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
